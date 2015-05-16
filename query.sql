54f42c2239f13ac78151fc605e36506ddb077f02
ironholds <ironholds@gmail.com>
2015-05-15 22:31:48 -0400

    start

---
+++ query.sql
@@ -0,0 +1,12 @@
+USE wmf;
+SELECT year, geocoded_data['country'] AS country, uri_query, user_agent, COUNT(*) AS requests
+FROM webrequest
+WHERE year = 2015
+AND month = 04
+AND ((day = 20 AND hour = 3) OR (day = 21 AND hour = 9) OR (day = 23 AND hour = 23) OR (day = 25 AND hour = 15)
+OR (day = 25 AND hour = 18))
+AND uri_path = '/w/api.php'
+AND uri_query RLIKE('action=opensearch')
+AND webrequest_source IN('text','mobile')
+AND http_status IN('200','304')
+GROUP BY geocoded_data['country'], uri_query, user_agent;
